9ad89232ec858fd7cd0eeb080f8c82c1115be19b
FarAK10 <botirovfarhod115@gmail.com>
2023-01-31 13:12:14 +0500

    feat: add nodemon.json

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import process from 'process';
 
+import studentRoutes from './src/student/routes';
 
-import studentRoutes from './src/student/rotues'
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -11,6 +11,6 @@ app.get('/',(req,res)=>{
     res.send('Hello postgress');
 });
 
-app.use('/api/v1/students',studentRoutes)
+app.use('/api/v1/students',studentRoutes);
 
 app.listen(PORT,()=>console.log('server started on prod'));
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,6 +4,6 @@ const router = Router();
 
 router.get('/',(req,res)=>{
     res.send('using api route');
-})
+});
 
 export default router;
